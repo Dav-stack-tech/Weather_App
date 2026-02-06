81d16b6206a75c7bd6a5bedb12cb92ff5dc8037f
Amani Musafiri David <amanimusafiri2007@gmail.com>
2026-02-06 23:53:05 +0200

    second commit
    working on the navbar

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+const cities = ["Goma", "Kinshasa", "Bukavu", "Kigali", "Kindu", "Lubumbashi"];
+const input = document.getElementById("city");
+const options = document.getElementById("options");
+const btnSearch = document.getElementById("btnSearch");
+
+input.addEventListener("input", () => {
+  const value = input.value.toLowerCase();
+  options.innerHTML = "";
+
+  if (!value) {
+    options.style.display = "none";
+    return;
+  }
+
+  options.style.display = "block";
+
+  const filtered = cities.filter((city) =>
+    city.toLowerCase().startsWith(value),
+  );
+  if (filtered.length == 0) {
+    const li = document.createElement("li");
+    li.textContent = "No information!";
+    options.appendChild(li);
+  }
+
+  filtered.forEach((city) => {
+    const li = document.createElement("li");
+    li.textContent = city;
+    li.onclick = () => {
+      input.value = city;
+      options.style.display = "none";
+    };
+    options.appendChild(li);
+  });
+});
+
+btnSearch.addEventListener("click", verifyCity);
+
+function verifyCity() {
+  const value = input.value;
+
+  if (!value) {
+    alert("Search empty !");
+    return;
+  }
+}
--- partials/header.php
+++ partials/header.php
@@ -3,14 +3,36 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Feel The Weather</title>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+  />
+  <link rel="stylesheet" href="./../assets/css/style.css"/>
 </head>
 <body>
-  <nav class="header">
-    <a href="index.php" class="nav-link">Today</a>
-    <a href="forecast.php" class="nav-link">Forecast</a>
-    <a href="maps.php" class="nav-link">Maps</a>
-    <a href="alerts.php" class="nav-link">Alerts</a>
-  </nav>
+  <div class="nav-bar">
+    <div class="weather">
+      <div class="logo">
+        <img src="./../assets/icon/logo_weather.png" alt="weather's logo">
+      </div>
+      <div class="text">
+        <h2>FeelTheWeather</h2>
+      </div>
+    </div>
+    <nav class="header">
+      <a href="index.php" class="nav-link">Today</a>
+      <a href="forecast.php" class="nav-link">Forecast</a>
+      <a href="maps.php" class="nav-link">Maps</a>
+      <a href="alerts.php" class="nav-link">Alerts</a>
+    </nav>
+    <div class="search-bar">
+      <i id="btnSearch" class="fa-solid fa-magnifying-glass search-icon"></i>
+      <input class="input" type="text" name="city" id="city" placeholder="Search a city ..." autocomplete="off">
+      <ul id="options"></ul>
+    </div>
+  </div>
+
+  <script src="./../assets/js/main.js"></script>
 </body>
-</html>
+</html> 
